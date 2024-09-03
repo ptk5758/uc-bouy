5bfe4f829e8f50d9afce5bca067be8afcbef1a65
ptk57581 <ptk725739@gmail.com>
2024-09-03 10:21:51 +0900

    prompt publishing

--- src/routes/prompt/index.tsx
+++ src/routes/prompt/index.tsx
@@ -1,7 +1,35 @@
+import { Link } from "react-router-dom"
+import "../../css/prompt.css"
 export default function() {
     return (
-        <div>
-            프롬프트
+        <div className="prompt">
+            <div className="board">
+                <div className="top">
+                    <h1>Prompt</h1>
+                    <Link to={"/telemetry"}>Back</Link>
+                </div>
+                <Terminal/>
+            </div>
+            <div className="command">
+                <Input/>
+            </div>
         </div>
     )
+}
+
+function Input() {
+    return (
+        <div className="field">
+            <textarea></textarea>
+            <button>SEND</button>
+        </div>
+    )
+}
+
+function Terminal() {
+    return (
+        <div className="terminal">
+            뿌슝빠슝하였다
+        </div>
+    )    
 }
